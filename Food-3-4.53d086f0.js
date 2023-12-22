dcc514ba8f014ba24246ab25452a6a980fd3bb56
Svitlana-Hreshnova <133237028+Svitlana-Hreshnova@users.noreply.github.com>
2023-12-22 19:55:10 +0000

    Deploying to gh-pages from @ Svitlana-Hreshnova/Food-3-4@4077c8469e524473f07bf8d2e40729e88848e84a 🚀

--- Food-3-4.53d086f0.js
+++ Food-3-4.53d086f0.js
@@ -1,2 +1,2 @@
 const c=document.querySelectorAll(".nav-link");function e(e){c.forEach((c=>{c.classList.remove("active")})),e.target.classList.add("active")}c.forEach((c=>{c.addEventListener("click",e)}));
-//# sourceMappingURL=index.00a7c228.js.map
+//# sourceMappingURL=Food-3-4.53d086f0.js.map
